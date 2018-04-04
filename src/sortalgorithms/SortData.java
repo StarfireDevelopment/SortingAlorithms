30d2ae0221400f339c4b9a16d711df30fc3a9ac5
Dariusz Szoltys <dariusz.26@hotmail.com>
2018-04-04 18:08:10 +0100

    Quicksort finished.

--- src/sortalgorithms/SortData.java
+++ src/sortalgorithms/SortData.java
@@ -15,7 +15,7 @@ public class SortData {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[] data = {2,4,5,2,1,1,15,8,8};
+        int[] data = {2,4,5,2,1,1,15,8,8,7,7,7,13,101,-13,2};
         QuickSort.quickSort(data);
     }
     
